e2528030f2111e09dd696560c2065342bbfa9e79
Сергей <040620@mail.ru>
2022-01-14 18:57:12 +0300

    Initial commit

---
+++ src/main/java/pro/sky/webstore/skyprowebstore/SkyProWebStoreApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.webstore.skyprowebstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProWebStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProWebStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/webstore/skyprowebstore/SkyProWebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.webstore.skyprowebstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProWebStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
